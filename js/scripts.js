11465be8f7567ebda1674f248a5bb41abdf5cb9c
sb <stevevburton@gmail.com>
2016-10-28 16:40:21 -0700

    still can't output the numbers counting up to the number input

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,29 @@
 // var digits = function(number) {
 //   var number = number.split("");
 // };
-var pingPong = function(number) {
-  //var number = parseInt(splitNumber)
-  for(var index=1; index<=number.length; index +=1) {
-    if(number % 15 === 0){
-      return ("ping-pong");
-		} else if(number % 3 === 0){
-			return("ping");
-		} else if(number % 5 === 0){
-			return("pong");
-		} else {
-      numberString = number.toString(" ");
-      digits = numberString.split(",");
-      return digits;
+// var numberArray = function(arraySize) {
+//   var digit = [];
+//   digit.push(number.length);
+// };
+var pingPong = function(userNumber) {
+//  var numberString = digit.toString("");
+  for(var index = 1; index <= userNumber; index ++) {
+    //  var digit = [];
+    //  digit.push(number);
+     if (userNumber % 15 === 0) {
+		   return ("ping-pong");
+		 } else if (userNumber % 5 === 0) {
+			 return ("pong");
+		 } else if (userNumber % 3 === 0) {
+       return ("ping");
+		 } else {
+       return userNumber;
+      // numberString = number.toString(" ");
+      // digits = numberString.split(",");
 		}
   }
 };
+
 
 
 
@@ -26,7 +33,7 @@ var pingPong = function(number) {
 $(document).ready(function() {
   $("form#inputBox").submit(function(event){
       event.preventDefault();
-      var userNumber = $("input#numberInput").val();
+      var userNumber = parseInt($("input#numberInput").val());
       var result = pingPong(userNumber);
       $("#convertResult").text(result);
     });
