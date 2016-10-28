8e4f359cf3918583a4f41d8c5dba2d275c66c261
sb <stevevburton@gmail.com>
2016-10-28 09:09:18 -0700

    add javascript to return a user inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
 
-function pingPong() {
-	for(var number=1; number<=100; number++){
-		if(number % 3 === 0 && number % 5 === 0){
-			return("ping-pong");
-		} else if(number % 3 === 0){
-			return("ping");
-		} else if(number % 5 === 0){
-			return("pong");
-		} else {
-			return(i);
-		}
-	}
-}
+var pingPong = function(number) {
+  var number = userNumber;
+  for(var index=1; index<=100; index +=1) {
+    return (number);
+  }
+
+};
+
+// function pingPong(userNumber) {
+//   var number = userNumber;
+// 	for(var number=1; number<=100; number++){
+// 			return(number);
+// 		}
+// 	}
+
 
 
 // User logic:
-$("#inputBox").submit(function(event){
-    event.preventDefault();
-    var userNumber = parseInt($("#numberInput").val());
-
-    var result = inputCheck(userNumber);
-    $("#numeralResult").text(result);
+$(document).ready(function() {
+  $("form#inputBox").submit(function(event){
+      event.preventDefault();
+      var userNumber = parseInt($("input#numberInput").val());
+      var result = pingPong(userNumber);
+      $("#convertResult").text(result);
+    });
   });
-});
